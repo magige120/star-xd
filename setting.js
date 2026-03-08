1ff18aaf9d6bdbc36b8ff3b1d083f7008723e5f5
magige120 <120magige@gmail.com>
2026-03-08 13:53:50 +0300

    Update bot and owner information in settings

--- setting.js
+++ setting.js
@@ -1,13 +1,13 @@
 //=====💫 sᴛᴀʀ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'MR CYBER BOT', // ur bot name
+  OWNER_NAME: 'Cyberpot master', //your name
+  OWNER_NUMBER: '255782102666', // add your phone number
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
-  SESSION_ID: '', // add your session ID
+  SESSION_ID: 'king~NUgF2a', // add your session ID
   PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
